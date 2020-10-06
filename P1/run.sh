0fb15b8340444da6493a7785c78efea93117b435
Matt Strapp <strap012@umn.edu>
2020-10-05 22:24:26 -0500

    Remove bad file

--- P1/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-make && cp mapper solutionexe/mapper && cp mapreduce solutionexe/mapreduce && cd solutionexe/ && make run && cp mapper.good mapper && cp mapreduce.good mapreduce && cd ..
